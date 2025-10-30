7d8e7134f6d8c6be7b140554633acc943aa4735d
Ishrat Jahan <ishrat.jahan16@tafensw.edu.au>
2025-10-30 15:22:33 +1100

    test caseadded

--- Lab1.js
+++ Lab1.js
@@ -1,6 +1,6 @@
-import readlinesync from 'readlne-sync'
-let myName=readlinesync.question('What is your name?')
-console.log('welcome to introduction to devops'+myName)
+import readlinesync from 'readlne-sync';
+let myName=readlinesync.question('What is your name?');
+console.log('welcome to introduction to devops'+myName);
 console.log('Hello World')
 console.log('Test')
 console.log('Hello World')
