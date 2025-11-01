edce282a65c7051c1aa2a55b8d634cf45ce69a17
Ishrat Jahan <ishrat.jahan16@tafensw.edu.au>
2025-11-02 07:13:49 +1100

    add user docker and yml file modified

--- Lab1.js
+++
@@ -1,8 +0,0 @@
-import readlinesync from 'readlne-sync';
-let myName=readlinesync.question('What is your name?');
-console.log('welcome to introduction to devops'+myName);
-console.log('Hello World')
-console.log('Test')
-console.log('Hello World')
-console.log('Test')
-//sample branch
